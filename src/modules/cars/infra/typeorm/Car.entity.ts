b42144decdb4090121c3d7318fc8148e8ade00d9
Joao Rangel <joao.vran@gmail.com>
2022-09-21 10:23:48 -0300

    fix: update type or entities with relations

--- src/modules/cars/infra/typeorm/Car.entity.ts
+++ src/modules/cars/infra/typeorm/Car.entity.ts
@@ -11,6 +11,8 @@ import {
 import {User} from '@modules/users/infra/typeorm/User.entity';
 import {Schedule} from '@modules/schedules/infra/typeorm/Schedule.entity';
 import {ICar} from '@modules/cars/interfaces/ICar';
+import {IUser} from '@modules/users/interfaces/IUser';
+import {ISchedule} from '@modules/schedules/interfaces/ISchedule';
 
 @Entity('cars')
 export class Car implements ICar {
@@ -25,11 +27,11 @@ export class Car implements ICar {
 
   @ManyToOne(() => User)
   @JoinColumn({name: 'userId'})
-  user: User;
+  user: IUser;
 
   @OneToMany(() => Schedule, (schedule) => schedule.car)
   @JoinColumn()
-  schedules: Schedule[];
+  schedules: ISchedule[];
 
   @CreateDateColumn()
   createdAt: Date;
